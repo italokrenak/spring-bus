1b026c8565b71a2c8809eaceeb3edcaaa4f67cfc
italokrenak <italokrenak@gmail.com>
2018-02-17 19:22:20 -0300

    Aplicação utilizando spring cloud bus

---
+++ client-solution/src/main/java/com/example/bus/ClientSolutionApplication.java
@@ -0,0 +1,12 @@
+package com.example.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSolutionApplication.class, args);
+	}
+}
